316ce82eceb44b36c007a5c222241eadcba8e4b6
mochrira <mochrira@gmail.com>
2020-06-29 14:20:00 +0700

    add uri to retrieve base url

---
+++ helpers/Uri.php
@@ -0,0 +1,10 @@
+<?php 
+
+function base_url() {
+    return sprintf(
+        "%s://%s%s",
+        isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+        $_SERVER['SERVER_NAME'],
+        $_SERVER['REQUEST_URI']
+    );
+}
