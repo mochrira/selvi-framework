dc4c968597aaa729d4b6fba5a072c3b15629b8c4
Moch Rizal Rachmadani <mochrira@gmail.com>
2021-03-26 15:24:13 +0700

    #3 uri library improvements

--- src/Uri.php
+++ src/Uri.php
@@ -7,10 +7,15 @@ class Uri {
     private $uri;
     private $segments;
 
+    private $subFolder;
+    private $scriptName;
+
     public function __construct() {
-        $sPos = strrpos($_SERVER['SCRIPT_NAME'], basename($_SERVER['SCRIPT_NAME']));
-        $sDir = substr($_SERVER['SCRIPT_NAME'], 0, $sPos);
-        $this->uri = preg_replace('/'.preg_quote($sDir, '/').'/', '/', $_SERVER['REQUEST_URI'], 1);
+        $this->scriptName = $_SERVER['SCRIPT_NAME'];
+        $sPos = strrpos($this->scriptName, basename($this->scriptName));
+        $this->subFolder = substr($this->scriptName, 0, $sPos);
+        $this->uri = preg_replace('/'.preg_quote($this->subFolder, '/').'/', '/', $_SERVER['REQUEST_URI'], 1);
+        var_dump($this->base_url());
         $has_query = strpos($this->uri, '?');
         if($has_query !== false) {
             $this->uri = substr($this->uri, 0, $has_query);
@@ -26,5 +31,15 @@ class Uri {
 	{
 		return isset($this->segments[$index - 1]) ? $this->segments[$index - 1] : null;
 	}
+
+    public function base_url() {
+        return sprintf(
+            "%s://%s%s%s",
+            isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+            $_SERVER['SERVER_NAME'],
+            isset($_SERVER['SERVER_PORT']) ? ':' . $_SERVER['SERVER_PORT'] : '',
+            $this->subFolder
+        );
+    }
 
 }
