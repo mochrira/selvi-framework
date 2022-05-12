321229418a67b8778a437a55b455c09bce46ddda
mochrira <mochrira@gmail.com>
2022-05-12 14:13:52 +0700

    Remove only array and traversables warning on Factory

--- src/Factory.php
+++ src/Factory.php
@@ -4,16 +4,19 @@ namespace Selvi;
 
 class Factory{
     
-    private static $object = array();
+    private static $object = [];
     
-    public static function load($name, $params = array(), $customName = ''){
+    public static function load($name, $params = [], $customName = ''){
         $realName = $name;
+        if(is_string($params)) {
+            $customName = $params;
+        }
         if(strlen($customName) > 0) {
             $realName = $customName;
         }
-
+        
         if(!isset(self::$object[$realName])){
-            self::$object[$realName]=new $name(...$params);
+            self::$object[$realName]=new $name(...(is_array($params) ? $params : []));
         }
         return self::$object[$realName];
     }
