f08c389e93716e35e60deb290836d5150f3076eb
mochrira <mochrira@gmail.com>
2023-11-01 10:05:45 +0700

    Revert "simple implementation of middleware"
    This reverts commit 835ed9c493e84b6073b1c0f23402841994f191ac.

--- examples/index.php
+++ examples/index.php
@@ -1,6 +1,12 @@
 <?php 
 
-define('BASEPATH', __DIR__);
 require 'vendor/autoload.php';
-Selvi\Route::get('/', 'HomeController@index');
+define('BASEPATH', __DIR__);
+
+$dbConfig = json_decode(file_get_contents(BASEPATH.'/private/.DBCONFIG'), true);
+Selvi\Database\Manager::add($dbConfig, 'main');
+Selvi\Database\Migration::addMigrations('main', [ BASEPATH.'/app/Migrations' ]);
+Selvi\Cli::register('migrate', Selvi\Database\Migration::class);
+
+require 'app/Config/routes.php';
 Selvi\Framework::run();
