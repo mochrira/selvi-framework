a4f8ff4ee13f3e08ab601b0554d35264a2090a45
mochrira <mochrira@gmail.com>
2024-03-06 09:26:24 +0700

    improve examples

--- examples/app/Controllers/HomeController.php
+++ examples/app/Controllers/HomeController.php
@@ -7,24 +7,24 @@ use Selvi\Database\Manager;
 
 class HomeController extends Controller {
 
-    private $db;
+    // private $db;
 
     function __construct() {
-        $this->db = Manager::add([
-            'host' => 'mariadb.database',
-            'username' => 'root',
-            'password' => 'RDF?jq8eec',
-            'database' => 'test'
-        ]);
+        // $this->db = Manager::add([
+        //     'host' => 'mariadb.database',
+        //     'username' => 'root',
+        //     'password' => 'RDF?jq8eec',
+        //     'database' => 'test'
+        // ]);
     }
 
     function index() {
-        $this->db->where([['kontak.idKontak', 1]]);
-        $this->db->update('kontak', [
-            'nmKontak' => "Yayasan Al Qur'an",
-            'noHp' => 'App\Contact\Quran',
-            'penjualan' => 15000.89
-        ]);
+        // $this->db->where([['kontak.idKontak', 1]]);
+        // $this->db->update('kontak', [
+        //     'nmKontak' => "Yayasan Al Qur'an",
+        //     'noHp' => 'App\Contact\Quran',
+        // ]);
+        //     'penjualan' => 15000.89
         return response('Halo');
     }
     
